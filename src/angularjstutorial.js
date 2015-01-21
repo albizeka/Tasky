8e5e0462262ee64e9e85983da6585e9ce6d3d540
unknown <albiizeka@gmail.com>
2015-01-21 23:58:14 +0100

    The working envoirment

---
+++ src/angularjstutorial.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Zeka on 21.1.2015.
+ * Umderstand Angular
+ */
+
+var app = angular.module("App" , []);
+
+app.controller("registerCtrl" , function($scope){
+    $scope.message = "btn-default";
+})
+
+
+
