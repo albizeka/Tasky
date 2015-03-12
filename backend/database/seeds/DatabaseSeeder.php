7f24625c3e72abbadee102b457296e28c7d94e0e
unknown <albiizeka@gmail.com>
2015-03-12 23:03:34 +0100

    Some of fixing files

--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Model::unguard();
+		Eloquent::unguard();
 
 		 $this->call('UserTableSeeder');
 	}
--- backend/database/seeds/UserTableSeeder.php
+++ backend/database/seeds/UserTableSeeder.php
@@ -1,16 +1,17 @@
 <?php
 
-use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Model;
+class UserTableSeeder extends Seeder
+{
 
-class UserTableSeeder extends Seeder {
-    public function run(){
-        DB::table('users')->delete();
- 
-        User::create(array(
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('admin')
-        ));
-    }
+public function run()
+{
+    DB::table('users')->delete();
+    User::create(array(
+        'name'     => 'Chris Sevilleja',
+        'username' => 'sevilayha',
+        'email'    => 'chris@scotch.io',
+        'password' => Hash::make('awesome'),
+    ));
 }
-?>
+
+}
