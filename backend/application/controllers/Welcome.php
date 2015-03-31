2f18c7583f705da88ab30ef0bf65209ffab57a67
unknown <albiizeka@gmail.com>
2015-03-31 19:18:03 +0200

    backend login logic done

--- backend/application/controllers/Welcome.php
+++ backend/application/controllers/Welcome.php
@@ -3,23 +3,84 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+	
+
+	public function __construct() {
+		parent::__construct();
+
+		// Load form helper library
+		$this->load->helper('form');
+
+		// Load form validation library
+		$this->load->library('form_validation');
+
+		// Load session library
+		$this->load->library('session');
+
+		// Load database
+		$this->load->model('login_database');
+
+	}
+
 	public function index()
 	{
 		$this->load->view('welcome_message');
+	}
+
+	//logic of login form
+	public function login(){
+
+	//	$this->load->model('login_database');
+
+		$this->form_validation->set_rules('username', 'Username', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+
+		if($this->form_validation->run()){
+			$data = array(
+				'username'=> $this->input->post('username'),
+				'password'=> $this->input->post('password')
+				);
+
+			$result = $this->login_database->login($data);
+			if($result == TRUE){
+				$sess_array = array(
+					'username'=>$this->input->post('username')
+				);
+			}
+
+			// add user data into session 
+			$this->session->set_userdata('logged_id',$sess_array);
+			$result = $this->login_database->read_user_information($sess_array);
+
+			if($result != false){
+				$data = array(
+					'username'=>$result[0]->username,
+					'password'=>$result[0]->password
+				);
+			
+			$this->load->view('dashboard');	
+			
+			} else {
+				$data = array(
+					'errors_message'=> 'Invalid username or password'
+				);
+
+				$this->load->view('welcome_message' , $data);
+			}
+		} else {
+			echo "wrong";
+		}
+	}
+
+	//logout
+	public function logout(){
+		//Removing session
+		$sess_array = array(
+			'username'=>''
+		);
+
+		$this->session->set_userdata('logged_id',$sess_array);
+		$data['message_display'] = "Succesfuly logout";
+		$this->load->view('welcome_message',$data);
 	}
 }
