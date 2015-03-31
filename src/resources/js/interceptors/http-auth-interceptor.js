d4b0415a2983bc629d3a6578b639879b609b9761
unknown <albiizeka@gmail.com>
2015-03-31 18:25:49 +0200

    lastet update that we make in frontend and backend

--- src/resources/js/interceptors/http-auth-interceptor.js
+++ src/resources/js/interceptors/http-auth-interceptor.js
@@ -1,33 +1,3 @@
-var app = angular.module('tasky' , ['ui.router']);
-
-'use strict';
-
-app.config(function($stateProvider, $urlRouterProvider) {
-
-	$urlRouterProvider.otherwise('/login');
-
-	$stateProvider
-		.state('login' , {
-			url : '/login',
-			templateUrl:'public/views/login.html'
-		})
-		.state('register' , {
-			url : '/register',
-			template : "<h1>Register here</h1>"
-		})
-
-});
-
-'use strict';
-
-app.controller('LoginController' , function ($scope){
-	
-});
-
-App.controller("RegisterController" , function ($scope){
-	$scope.message = "Register";
-})
-
 /*global angular:true, browser:true */
 
 /**
