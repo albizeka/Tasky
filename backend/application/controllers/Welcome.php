a36370d71707e8053ed316fbbbb513e31ee7dc04
Albi Zeka <albizeka@users.noreply.github.com>
2015-10-21 18:29:46 +0200

    Update Welcome.php
    remove of comment code

--- backend/application/controllers/Welcome.php
+++ backend/application/controllers/Welcome.php
@@ -32,51 +32,6 @@ class Welcome extends CI_Controller {
 	//logic of login form
 	public function login(){
 
-	//	$this->load->model('login_database');
-
-		// $this->form_validation->set_rules('mail', 'Mail', 'required');
-		// $this->form_validation->set_rules('password', 'Password', 'required');
-
-		// if($this->form_validation->run()){
-		// 	$data = array(
-		// 		'mail'=> $this->input->post('mail'),
-		// 		'password'=> $this->input->post('password')
-		// 		);
-		// 	print_r($data);
-
-		// 	$result = $this->login_database->login($data);
-		// 	if($result == TRUE){
-		// 		// $sess_array = array(
-		// 		// 	'mail'=>$this->input->post('mail')
-		// 		// );
-		// 		session_start();
-		// 		$_SESSION['id'];
-		// 		print $_SESSION['id'];
-		// 	}
-
-		// 	// add user data into session 
-		// 	$this->session->set_userdata('logged_id',$sess_array);
-		// 	$result = $this->login_database->read_user_information($sess_array);
-
-		// 	if($result != false){
-		// 		$data = array(
-		// 			'mail'=>$result[0]->mail,
-		// 			'password'=>$result[0]->password
-		// 		);
-			
-		// 	$this->load->view('dashboard');	
-			
-		// 	} else {
-		// 		$data = array(
-		// 			'errors_message'=> 'Invalid mail or password'
-		// 		);
-
-		// 		$this->load->view('welcome_message' , $data);
-		// 	}
-		// } else {
-		// 	echo "wrong";
-		// }
-
 		$postdata = file_get_contents("php://input");
 
 		$request = json_decode($postdata);
